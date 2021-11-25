75127e9fb53f088754393e6bd7c050814f46a4be
Cleo Menezes Jr <produtor.junior.menezes@gmail.com>
2021-11-25 01:02:36 -0300

    Chore: Adding first post test.

---
+++ home/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
+from .helpers import *
+
+
+class BlogModel(models.Model):
+    title = models.CharField(max_length=1000)
+    content = FroalaField()
+    slug = models.SlugField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(upload_to='blog')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upload_to = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(BlogModel, self).save(*args, **kwargs)
